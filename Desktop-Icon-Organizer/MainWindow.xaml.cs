08549fad6883a56a45ab75fb472f9fcd93a31314
Tomas <tomasmileto@gmail.com>
2021-03-22 18:54:39 -0300

    Finished with the basic WFP setup for the app
    Used XAML and the View Pane to edit the style. Also added the "select folder" functionality using the NuGet Pacakge "Windows API CodePack Shell"

---
+++ Desktop-Icon-Organizer/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop_Icon_Organizer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new CommonOpenFileDialog();
+            dlg.Title = "Destination Folder Dialog";
+            dlg.IsFolderPicker = true;
+            dlg.InitialDirectory= Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems = false;
+            dlg.DefaultDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dlg.EnsureFileExists = true;
+            dlg.EnsurePathExists = true;
+            dlg.EnsureReadOnly = false;
+            dlg.EnsureValidNames = true;
+            dlg.Multiselect = false;
+            dlg.ShowPlacesList = true;
+
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var folder = dlg.FileName;
+                txtSelectedFolder.Text = folder;
+                // Do something with selected folder string
+            }
+        }
+    }
+}
